378d2931162f54dd655cb6bdb19a67cd5d68d969
Precious Oranye <79994012+Preshfile@users.noreply.github.com>
2024-01-29 21:53:39 +0100

    Update fs.js

--- HTTP GET request NodeJs/Backend/fs.js
+++ HTTP GET request NodeJs/Backend/fs.js
@@ -1,17 +1,8 @@
-//File System Module - used to read a file unto a screen or into a program or write into a particular file
-const fs = require("fs"); //importing and using a file system model
 
-/*
-fs.readFile('Readme.txt', function(err, data){
-    console.log(data); //This output the text file content encrypted in buffer
-});
+const fs = require("fs"); 
 
-*/
-
-//Another way reading a text file
-
-const data = fs.readFileSync("Readme.txt", "utf-8"); //This is how you display a file on a page
-console.log(data); //this will output file content without encryption
+const data = fs.readFileSync("Readme.txt", "utf-8"); 
+console.log(data); 
 
 const data2 = fs.readFileSync("datasource.json", "utf-8");
 console.log(data);
